2893e51046d732ecf788d90755338828c941233f
Leonardo Silva <leonardo.gslva@gmail.com>
2026-08-07 15:21:20 -0300

    adicionar funcao de movimento

--- player/script/player.gd
+++ player/script/player.gd
@@ -7,6 +7,23 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const NEGATIVE_SPEED = 2000
 
+func player_animations(direction):
+		if Input.is_action_just_pressed("left"):
+			sprite_2d.flip_h = true 
+		if Input.is_action_just_pressed("right"):
+			sprite_2d.flip_h = false 
+
+		if is_on_floor():
+			if direction != 0:
+				animation_player.play("run")
+			if direction == 0:
+				animation_player.play("idle")
+	
+		elif velocity.y < 0:
+			animation_player.play("jump")
+		elif velocity.y > 0:
+			animation_player.play("fall")
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,23 +32,17 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animation_player.play("jump")
-	elif is_on_floor():
-		animation_player.play("idle")
+		print(JUMP_VELOCITY);
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	player_animations(direction)
+	
 	if direction:
 		velocity.x = direction * SPEED
-		animation_player.play("run")
-		if direction == -1:
-			sprite_2d.flip_h = true 
-		else: 
-			sprite_2d.flip_h = false 
 	else:
 		velocity.x = move_toward(velocity.x, 0, NEGATIVE_SPEED * delta)
-		animation_player.play("idle") 	
-	
+
 
 	move_and_slide()
